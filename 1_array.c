0cb760c72830805783b3036758f599309898701b
Jash-Pal <jashpal2112@gmail.com>
2022-03-01 18:59:58 +0530

    C Revision

---
+++ 1_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[4] = {3,4,2,7};
+    int i;
+    for (i=0 ; i<4 ; i++)
+    {
+        printf(" \n %d",a[i]); 
+    }
+    
+printf("\n");
+
+    int x;
+    int y;
+    scanf("%d %d"  ,&x , &y);
+    int add = x + y;
+    printf("\n my sum is %d \n",add);
+    return 0;
+}
+
+
+
+
+
